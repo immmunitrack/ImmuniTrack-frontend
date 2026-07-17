d0e6e360d451d16337a9d4f3158d3eecf7763aeb
mulisa888 <mulisadocile@gmail.com>
2026-07-17 10:37:51 +0300

    fix api reading in frontend to fix runtime production bug

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log("Current API URL:", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || window.VITE_API_URL || window.ENV?.VITE_API_URL || (import.meta.env.PROD ? '' : 'http://localhost:5050/api')
 });
